446bf152069244cd7a82b8453e518f34507f7817
ramimdev <ourislamBD247@gmail.com>
2025-07-25 15:17:04 +0600

    Update cf.js

--- js/cf.js
+++ js/cf.js
@@ -1,7 +1,7 @@
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({&#39;gtm.start&#39;:
-new Date().getTime(),event:&#39;gtm.js&#39;});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!=&#39;dataLayer&#39;?&#39;&amp;l=&#39;+l:&#39;&#39;;j.async=true;j.src=
-&#39;https://www.googletagmanager.com/gtm.js?id=&#39;+i+dl+&#39;&amp;c=&#39;+Math.random();f.parentNode.insertBefore(j,f);
-})(window,document,&#39;script&#39;,&#39;dataLayer&#39;,&#39;GTM-MQ4ZCK59&#39;);</script>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&amp;c='+Math.random();f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MQ4ZCK59');</script>
 <!-- End Google Tag Manager -->
