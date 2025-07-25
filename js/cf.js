585942bcbb9cd7995a6e97730612fe117f47f8ab
ramimdev <ourislamBD247@gmail.com>
2025-07-25 15:00:57 +0600

    Create cf.js

---
+++ js/cf.js
@@ -0,0 +1,7 @@
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({&#39;gtm.start&#39;:
+new Date().getTime(),event:&#39;gtm.js&#39;});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!=&#39;dataLayer&#39;?&#39;&amp;l=&#39;+l:&#39;&#39;;j.async=true;j.src=
+&#39;https://www.googletagmanager.com/gtm.js?id=&#39;+i+dl+&#39;&amp;c=&#39;+Math.random();f.parentNode.insertBefore(j,f);
+})(window,document,&#39;script&#39;,&#39;dataLayer&#39;,&#39;GTM-MQ4ZCK59&#39;);</script>
+<!-- End Google Tag Manager -->
